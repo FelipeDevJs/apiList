feecc9aaefa9cc764c93f99aea93b73a8deaec9c
FelipeJs <sr.felipe.viana@gmail.com>
2020-07-10 19:46:15 -0300

    api list

---
+++ App.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { FlatList, ActivityIndicator, Text, View, ScrollView, RefreshControl, StyleSheet  } from 'react-native';
+
+export default class FetchExample extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={ 
+      refreshing: false,
+    };
+  }
+
+  _onRefresh = () => {
+    this.setState({refreshing: true});
+    componentDidMount().then(() => {
+      this.setState({refreshing: false});
+    });
+  }
+
+  componentDidMount(){
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
+
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return(
+      <ScrollView
+        contentContainerStyle={styles.scrollview}
+        refreshControl={
+          <RefreshControl 
+          refreshing={this.props.refreshing}
+          onRefresh={this._onRefresh.bind(this)} />
+        }
+      >
+        <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={({id}, index) => id}
+        />
+      </View>
+      </ScrollView>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  scrollview:{
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
